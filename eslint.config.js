64fb7c4d823d59a277432de83e620b7f5ea54a14
tuliomitico <tulioneedforspeed@gmail.com>
2025-05-13 14:43:26 -0300

    feat: add first value object

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+]);
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,22 +1,22 @@
-import { test, expect } from 'vitest'
-import { AnswerQuestionUseCase } from './answer-question'
-import type { AnswersRepository } from '../repositories/answers-repository'
-import type { Answer } from '../entities/answer'
+import { test, expect } from "vitest";
+import { AnswerQuestionUseCase } from "./answer-question";
+import type { AnswersRepository } from "../repositories/answers-repository";
+import type { Answer } from "../entities/answer";
 
 const fakeAnswerRepository: AnswersRepository = {
-    create: async function (answer: Answer) {
-        return
-    }
-}
+  create: async function (answer: Answer) {
+    return;
+  },
+};
 
-test('create an answer', async () => {
-    const answer = new AnswerQuestionUseCase(fakeAnswerRepository)
+test("create an answer", async () => {
+  const answer = new AnswerQuestionUseCase(fakeAnswerRepository);
 
-    const result = await answer.execute({
-        instructorId: '1',
-        questionId: '1',
-        content: 'Nova resposta'
-    })
+  const result = await answer.execute({
+    instructorId: "1",
+    questionId: "1",
+    content: "Nova resposta",
+  });
 
-    expect(result.content).toEqual('Nova resposta')
-})
+  expect(result.content).toEqual("Nova resposta");
+});
